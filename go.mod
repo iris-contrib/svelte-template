529afafa81b809effcfea1ba7fc66938074297e8
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2020-09-13 22:04:00 +0300

    iris v12.2.0-alpha

--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module github.com/iris-contrib/svelte-template
 
-go 1.13
+go 1.15
 
 require (
-	github.com/kataras/iris/v12 v12.1.6
+	github.com/kataras/iris/v12 v12.2.0-alpha
 )
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import "github.com/kataras/iris/v12"
 
 func main() {
 	app := iris.New()
-	app.HandleDir("/", "./app/public")
-
-	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
+	app.HandleDir("/", iris.Dir("./app/public"))
+	app.Listen(":8080")
 }
